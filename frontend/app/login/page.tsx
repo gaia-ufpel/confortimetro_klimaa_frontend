49eafdc0f4e883072ee2e987d82445d1a99bbf40
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-08-19 04:11:02 -0300

    integrating input validation with zod, not done yet

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,19 +3,49 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { MdLock } from "react-icons/md";
 import { FiAtSign } from "react-icons/fi";
+import { MdOutlineReportGmailerrorred } from "react-icons/md";
+import { GrValidate } from "react-icons/gr";
 import { useParams, usePathname } from 'next/navigation';
 import Link from 'next/link';
+import TimedPopup from '../timed_popup';
 
 const LOGIN = () => {
   const [credentials, setCredentials] = useState({
     email: '',
     password: ''
   })
+  const [loginError, setLoginError] = useState<any>('')
 
-  const sendCredentials = async () => { }
+  const sendCredentials = async () => {
+    try {
+      const response = await fetch('/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: credentials.email, password: credentials.password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao fazer login');
+      }
+
+      const data = await response.json();
+      console.log('Login bem-sucedido:', data);
+      // Faça algo com os dados recebidos, como armazenar o token de autenticação
+    } catch (error) {
+      setLoginError(error)
+      console.error('Erro:', error);
+    }
+  };
+
+  useEffect(() => {
+    setLoginError('Erro ao logar'), []})
+
   const pathname = usePathname()
   return (
-    <div className='flex flex-col justify-center items-center font-montserrat'>
+    <div className='flex flex-col justify-center items-center font-montserrat min-h-screen'>
+      {loginError && <TimedPopup title={"Login Error"} message={loginError} className={"absolute right-0 bottom-0 m-4 text-2xl font-bold font-montserrat text-center"} />}
 
       <form className='flex flex-col space-y-4 justify-center items-center p-10'>
         <Image src="/login.png" width={180} height={180} alt='login image' />
@@ -28,12 +58,12 @@ const LOGIN = () => {
             <input type='password' className='block text-[24px] tracking-wide placeholder-stone-500 w-96 h-16 pr-10 pl-8 appearance-none bg-zinc-300 rounded-[23px] shadow-[0px_4px_4px_#00000040] focus:outline-none peer' placeholder="Insira sua senha" required onChange={(ev) => { setCredentials({ ...credentials, password: ev?.target.value }) }} />
             <MdLock className='absolute text-stone-500 right-3 pointer-events-none h-6 w-6' />
           </div>
-          <button className='absolute right-0 bottom-0 text-zinc-100 text-base font-semibold tracking-[0]'> Esqueci minha senha</button>
+          <button className='absolute right-0 bottom-0 text-zinc-100 text-base font-semibold tracking-[0] hover:underline hover:decoration-solid'> Esqueci minha senha</button>
         </div>
-        <button className='px-10 py-2 rounded-xl text-neutral-50 bg-[#885AC6] shadow-md border-2 border-green-300 font-extrabold text-2xl' onClick={sendCredentials}> Entrar </button>
+        <button className='px-10 py-2 rounded-xl text-neutral-50 bg-[#885AC6] hover:bg-[#926ec2] shadow-md border-2 border-green-300 font-extrabold text-2xl' onClick={sendCredentials}> Entrar </button>
       </form>
       <div className='flex flex-col space-y-10'>
-        <button className='text-zinc-100 text-base font-semibold' onClick={() => { }}>Ainda não possui uma conta?</button>
+        <div className='text-zinc-100 text-base font-semibold pointer-events-none'>Ainda não possui uma conta?</div>
         <Link href={`${pathname}/registrar`}>
           <button className='px-10 py-2 bg-[#78DF8C] opacity-75 text-neutral-50 text-xl font-semibold rounded-3xl border-2 border-violet-500 shadow-lg hover:opacity-100 duration-200'>Crie uma conta</button>
         </Link>
--- frontend/app/login/registrar/page.tsx
+++ frontend/app/login/registrar/page.tsx
@@ -2,44 +2,61 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { MdLock } from 'react-icons/md';
-const REGISTRAR = () => {
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form';
 
-    const [credentials, setCredentials] = useState({
-        name: '',
-        email: '',
-        password: '',
-        newpassword: '',
-    })
+import { signupSchema, TsignupSchema } from '@/lib/types';
+
+const REGISTRAR = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset,
+    } = useForm<TsignupSchema>({
+        resolver: zodResolver(signupSchema),
+    });
+
+
+    const onSubmit = async (data: TsignupSchema) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.log(data);
+    }
 
     return (
         <div className='flex flex-col min-h-screen min-w-screen items-center justify-center space-y-20'>
-            <form action="" className='font-montserrat'>
+            <form className='font-montserrat' onSubmit={handleSubmit(onSubmit)}>
                 <div className='grid space-y-4'>
                     <div className='space-y-1'>
                         <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Nome completo</label>
-                        <input className=" pl-8 md:pr-10 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none" type='text' required onChange={(ev) => { setCredentials({ ...credentials, name: ev?.target.value }) }}></input>
+                        <input className={`pl-8 md:pr-10 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none ${errors.name ? "border-red-500" : ""}`} type='text' {...register('name', { required: true })} disabled={isSubmitting}></input>
+                        {errors.name && (<p className='text-red-500'>{`${errors.name.message}`}</p>)}
                     </div>
                     <div className='space-y-1'>
                         <label className='block text-neutral-700 text-[24px] font-normal pl-3'>E-mail</label>
-                        <input className=" pl-8 md:pr-10 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none" type='email' required onChange={(ev) => { setCredentials({ ...credentials, email: ev?.target.value }) }}></input>
+                        <input className={`pl-8 md:pr-10 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none ${errors.email ? "border-red-500" : ""}`} type='email' {...register('email', { required: true })} disabled={isSubmitting}></input>
+                        {errors.email && (<p className='text-red-500'>{`${errors.email.message}`}</p>)}
                     </div>
                     <div className='space-y-1'>
                         <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Senha</label>
                         <div className='relative flex items-center'>
-                            <input className=" pl-8 md:pr-16 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none" type='password' required onChange={(ev) => { setCredentials({ ...credentials, password: ev?.target.value }) }}></input>
+                            <input className={`pl-8 md:pr-16 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none ${errors.password ? "border-red-500" : ""}`} type='password' {...register('password', { required: true })} disabled={isSubmitting}></input>
                             <MdLock className='absolute text-stone-500 right-6 pointer-events-none h-7 w-7' />
                         </div>
+                        {errors.password && (<p className='text-red-500 z-10'>{`${errors.password.message}`}</p>)}
                     </div>
                     <div className='space-y-1'>
                         <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Confirme sua senha</label>
                         <div className='relative flex items-center'>
-                            <input className="pl-8 md:pr-16 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none" type='password' required onChange={(ev) => { setCredentials({ ...credentials, newpassword: ev?.target.value }) }}></input>
+                            <input className={`pl-8 md:pr-16 w-80 md:w-[710px] h-10 md:h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040] outline-none ${errors.confirmpassword ? "border-red-500" : ""}`} type='password' {...register('confirmpassword', { required: true })} disabled={isSubmitting}></input>
                             <MdLock className='absolute text-stone-500 right-6 pointer-events-none h-7 w-7' />
                         </div>
+                        {errors.confirmpassword && (<p className='text-red-500'>{`${errors.confirmpassword.message}`}</p>)}
                     </div>
                 </div>
             </form>
-            <button className=' block text-center font-montserrat h-12 px-10 py-2 bg-[#78DF8C] opacity-75 text-neutral-50 text-xl font-semibold rounded-3xl border-2 border-violet-500 shadow-lg hover:opacity-100 duration-200'>Crie uma conta</button>
+            <button className={`block text-center font-montserrat h-12 px-10 py-2 bg-[#78DF8C] opacity-75 text-neutral-50 text-xl font-semibold rounded-3xl border-2 border-violet-500 shadow-lg hover:opacity-100 duration-200 disabled:pointer-events-none`}>Crie uma conta</button>
             <Image src="/register.png" width={250} height={250} alt='register image' className='md:absolute md:left-10 md:bottom-10' />
         </div>
     )
