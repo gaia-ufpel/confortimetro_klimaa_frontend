b3f5dd1654acb07de5b19f9d18de1e69447705f2
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-09-25 00:18:16 -0300

    Homepage fixes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { navigate } from '../login/actions'
+import { Geologica } from 'next/font/google';
+
+const geologica = Geologica({
+  subsets: ['latin'],
+  weight: ["400"],
+});
 
 const Home = () => {
   return (
     <div className='flex min-h-screen min-w-screen justify-center items-center'>
-      <p className='font-mono font-2xl'>Bem vindo! Este é um projeto desenvolvido pelo GAIA na Universidade Federal de Pelotas(UFPel)</p>
+      <p className={`${geologica.className} font-2xl`}>Bem vindo! Este é um projeto desenvolvido pelo GAIA na Universidade Federal de Pelotas(UFPel).</p>
     </div>
   )
 }
